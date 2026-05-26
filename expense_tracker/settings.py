01acdcea359058acf6e05b5ba8ea237256906b54
Mansi Patel <sjpatel7777@gmail.com>
2026-05-26 20:55:27 +0530

    Temp debug

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-change-this-in-production-use-env-variable'
 
 # Debug mode - set to False in production
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 APPEND_SLASH =True
